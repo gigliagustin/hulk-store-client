f14ec809e4b830442170eaa5ca3dc2d87aa0b8cf
Agustin Gigli <giglimagustin@gmail.com>
2021-09-02 17:05:55 -0300

    commiting changes

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -1,50 +1,56 @@
-import {React, Fragment, useState} from 'react'
+import { React, Fragment, useState } from 'react'
 import { Button, Modal, Form } from 'react-bootstrap';
+import axios from "axios";
 
 export default function Session() {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
+  const [userData, setUserData] = useState({})
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <Fragment>
-        <Button variant="outline-light" onClick={handleShow}>
-          Iniciar sesión
-        </Button>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-        <Modal
-          show={show}
-          onHide={handleClose}
-          keyboard={false}
-          backdrop="static"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Inicio de sesión</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Ingrese su usuario:</Form.Label>
-                <Form.Control type="email" placeholder="Ingrese su email" />
-              </Form.Group>
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setUserData({ ...userData, [event.target.name]: event.target.value })
+  }
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña:</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" type="submit">
-              Iniciar Sesión
-            </Button>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancelar
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Fragment>
-    );
+  const login = () => {
+    console.log(process.env.REACT_APP_API_URL);
+  }
+
+  return (
+    <Fragment>
+      <Button variant="outline-light" onClick={handleShow}>
+        Iniciar sesión
+      </Button>
+
+      <Modal show={show} onHide={handleClose} keyboard={false} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>Inicio de sesión</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Ingrese su usuario:</Form.Label>
+              <Form.Control type="email" name="email" placeholder="Ingrese su email" value={userData.email} onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Contraseña:</Form.Label>
+              <Form.Control type="password" name="password" placeholder="password" value={userData.password} onChange={handleChange} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" type="submit" onClick={login}>
+            Iniciar Sesión
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Fragment>
+  );
 }
 
