cea85eae3967784e21ae9ecdf9725513f4f1499c
Agustin Gigli <giglimagustin@gmail.com>
2021-09-02 12:54:46 -0300

    Mejoro la vista de las cards en la store

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -11,77 +11,91 @@ export default function Store({setShow, setModalShow}) {
                             <Card style={{ width: '18rem', height:'24rem'}} className="shadow-lg">  
                             <Card.Header as="h3">Product_Name</Card.Header>
                             <Card.Body>
-                                <Card.Subtitle className="text-muted">Descripción</Card.Subtitle>
+                                <Card.Subtitle className="text-muted">Product_Description</Card.Subtitle>
                                 <Card.Text>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil perspiciatis accusantium quisquam enim beatae temporibus, ad possimus rem commodi.
                                 </Card.Text>
                                 <ListGroup>
-    <ListGroup.Item >Precio:<Badge pill bg="dark" className="ms-2" > $10.57 
+    <ListGroup.Item >Product_Price:<Badge pill bg="dark" className="ms-2" > $10.57 
         </Badge>
         </ListGroup.Item>
-    <ListGroup.Item>Unidades Disponibles
+    <ListGroup.Item>Product_Stock
         <Badge pill bg="dark" className="ms-2"> 10
         </Badge>
         </ListGroup.Item>
-    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
   </ListGroup>
                             </Card.Body>
                             <Card.Footer>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Añadir al carrito</Button>
                             </Card.Footer>
                             </Card>
                         </Col>
                         <Col>
                         <Card style={{ width: '18rem', height:'24rem'}} className="shadow-lg">  
+                            <Card.Header as="h3">Product_Name</Card.Header>
                             <Card.Body>
-                                <Card.Title>Descripción</Card.Title>
+                                <Card.Subtitle className="text-muted">Product_Description</Card.Subtitle>
                                 <Card.Text>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil perspiciatis accusantium quisquam enim beatae temporibus, ad possimus rem commodi.
                                 </Card.Text>
-                                <ListGroup className="list-groupflush">
-    <ListGroup.Item>Precio</ListGroup.Item>
-    <ListGroup.Item>Unidades Disponibles</ListGroup.Item>
-    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                                <ListGroup>
+    <ListGroup.Item >Product_Price:<Badge pill bg="dark" className="ms-2" > $10.57 
+        </Badge>
+        </ListGroup.Item>
+    <ListGroup.Item>Product_Stock
+        <Badge pill bg="dark" className="ms-2"> 10
+        </Badge>
+        </ListGroup.Item>
   </ListGroup>
                             </Card.Body>
                             <Card.Footer>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Añadir al carrito</Button>
                             </Card.Footer>
                             </Card>
                         </Col>
                         <Col>
                         <Card style={{ width: '18rem', height:'24rem'}} className="shadow-lg">  
+                            <Card.Header as="h3">Product_Name</Card.Header>
                             <Card.Body>
-                                <Card.Title>Descripción</Card.Title>
+                                <Card.Subtitle className="text-muted">Product_Description</Card.Subtitle>
                                 <Card.Text>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil perspiciatis accusantium quisquam enim beatae temporibus, ad possimus rem commodi.
                                 </Card.Text>
-                                <ListGroup className="list-groupflush">
-    <ListGroup.Item>Precio</ListGroup.Item>
-    <ListGroup.Item>Unidades Disponibles</ListGroup.Item>
-    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                                <ListGroup>
+    <ListGroup.Item >Product_Price:<Badge pill bg="dark" className="ms-2" > $10.57 
+        </Badge>
+        </ListGroup.Item>
+    <ListGroup.Item>Product_Stock
+        <Badge pill bg="dark" className="ms-2"> 10
+        </Badge>
+        </ListGroup.Item>
   </ListGroup>
                             </Card.Body>
                             <Card.Footer>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Añadir al carrito</Button>
                             </Card.Footer>
                             </Card>
                         </Col>
                         <Col>
                         <Card style={{ width: '18rem', height:'24rem'}} className="shadow-lg">  
+                            <Card.Header as="h3">Product_Name</Card.Header>
                             <Card.Body>
-                                <Card.Title>Descripción</Card.Title>
+                                <Card.Subtitle className="text-muted">Product_Description</Card.Subtitle>
                                 <Card.Text>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nihil perspiciatis accusantium quisquam enim beatae temporibus, ad possimus rem commodi.
                                 </Card.Text>
-                                <ListGroup className="list-groupflush">
-    <ListGroup.Item>Precio</ListGroup.Item>
-    <ListGroup.Item>Unidades Disponibles</ListGroup.Item>
-    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                                <ListGroup>
+    <ListGroup.Item >Product_Price:<Badge pill bg="dark" className="ms-2" > $10.57 
+        </Badge>
+        </ListGroup.Item>
+    <ListGroup.Item>Product_Stock
+        <Badge pill bg="dark" className="ms-2"> 10
+        </Badge>
+        </ListGroup.Item>
   </ListGroup>
                             </Card.Body>
                             <Card.Footer>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Añadir al carrito</Button>
                             </Card.Footer>
                             </Card>
                         </Col>
