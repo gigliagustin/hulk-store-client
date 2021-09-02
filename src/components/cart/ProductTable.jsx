dfb0b2b73fba4d5ce24aac60c548c29778663a8e
Agustin Gigli <giglimagustin@gmail.com>
2021-09-02 18:34:18 -0300

    "Trabajando en el carrito"

--- src/components/cart/ProductTable.jsx
+++ src/components/cart/ProductTable.jsx
@@ -1,84 +1,75 @@
 import React,{Fragment} from 'react'
-import { Container, Table, Button } from 'react-bootstrap';
+import { Container, Table, Button, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function ProductTable() {
+
+    const [products, setProducts] = useState([]);
+
+    async function handleGetCart() {
+        const tokken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjEzMTA3N2YzNGI0MWNlMGQ1YjVlNzY5IiwidXNlcl9lbWFpbCI6ImRpZWdvZmxvcmVzMTk5MUBnbWFpbC5jb20iLCJpYXQiOjE2MzA2MTM3OTQsImV4cCI6MTYzMDYyMDk5NH0.stcRlcLg0ovAoAsuNdhfgGVhfnTmajSML97aZ8gBUbI";
+        const products = await axios.get(process.env.REACT_APP_API_URL + '/cart', {
+            headers: {
+                    "X-ACCESS-TOKEN": tokken
+                }
+                });
+        console.log(products.data);
+        setProducts(products.data.cartDetail);
+    }
+
+    useEffect(() => {
+        handleGetCart();
+    }, []);
+        
+
     return (
         <Fragment>
             <Container className="mt-3 border">
-               <Table striped bordered hover responsive>
+               <Table className="my-3" striped bordered hover responsive>
                    <thead>
                     <tr>
-                        <th>Product_Code#</th>
-                        <th>Product_Name</th>
-                        <th>Product_Price</th>
-                        <th>Product_Quantity</th>
-                        <th>Product_Actions</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Acciones</th>
                     </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>32465</td>
-                            <td>NameOfTheProduct</td>
-                            <td>$$$$$$</td>
-                            <td>2</td>
-                            <td> 
-                                <Button variant="outline-warning" className="me-2" title="Editar la cantidad del producto">
-                                    <FontAwesomeIcon icon={faEdit} />
-                                </Button>
-                                <Button variant="outline-danger" title="Eliminar producto del carrito">
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>32465</td>
-                            <td>NameOfTheProduct</td>
-                            <td>$$$$$$</td>
-                            <td>2</td>
-                            <td> 
-                                <Button variant="outline-warning" className="me-2" title="Editar la cantidad del producto">
-                                    <FontAwesomeIcon icon={faEdit} />
-                                </Button>
-                                <Button variant="outline-danger" title="Eliminar producto del carrito">
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>32465</td>
-                            <td>NameOfTheProduct</td>
-                            <td>$$$$$$</td>
-                            <td>2</td>
-                            <td> 
-                                <Button variant="outline-warning" className="me-2" title="Editar la cantidad del producto">
-                                    <FontAwesomeIcon icon={faEdit} />
-                                </Button>
-                                <Button variant="outline-danger" title="Eliminar producto del carrito">
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>32465</td>
-                            <td>NameOfTheProduct</td>
-                            <td>$$$$$$</td>
-                            <td>2</td>
-                            <td> 
-                                <Button variant="outline-warning" className="me-2" title="Editar la cantidad del producto">
-                                    <FontAwesomeIcon icon={faEdit} />
-                                </Button>
-                                <Button variant="outline-danger" title="Eliminar producto del carrito">
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </td>
-                        </tr>
 
+                        {products.map(product => ( 
+                        <tr>
+                            <td>{product.product.name}</td>
+                            <td>{product.product.price}</td>
+                            <td>{product.quantity}</td>
+                            <td> 
+                                <Button variant="outline-warning" className="me-2" title="Editar la cantidad del producto">
+                                    <FontAwesomeIcon icon={faEdit} />
+                                </Button>
+                                <Button variant="outline-danger" title="Eliminar producto del carrito">
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </Button>
+                            </td>
+                        </tr>
+                        ))}
                     </tbody>
                     
                </Table>
             </Container>
+            <Modal>
+                <Modal.Header>
+                    <Modal.Title>Editar cantidad</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Modal body text goes here.</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary">Close</Button>
+                    <Button variant="primary">Save Changes</Button>
+                </Modal.Footer>
+            </Modal>
         </Fragment>
     )
 }
